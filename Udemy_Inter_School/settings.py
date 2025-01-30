52c2771c68dc8ab592c52c10ab92d0f4e8e2bffe
Amrita965 <amrita15-3792@diu.edu.bd>
2025-01-30 12:53:41 +0600

    Add Configuration for media files

--- Udemy_Inter_School/settings.py
+++ Udemy_Inter_School/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$wb#5hk=m&$h2pbsht-#qrj8+0^d6)e%9ji8m53xh_)&clkc7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['udemy-inter-school-3eou.onrender.com', 'localhost', '127.0.0.1']
 
@@ -142,7 +142,7 @@ if not DEBUG:
 
 MEDIA_URL = '/media/'
 
-# MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Authentication
 
