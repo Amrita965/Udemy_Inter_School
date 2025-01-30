29c04dbe512a8c67ae89c356d23a58912e463126
Amrita965 <amrita15-3792@diu.edu.bd>
2025-01-30 13:03:22 +0600

    update configuration

--- Udemy_Inter_School/settings.py
+++ Udemy_Inter_School/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$wb#5hk=m&$h2pbsht-#qrj8+0^d6)e%9ji8m53xh_)&clkc7r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['udemy-inter-school-3eou.onrender.com', 'localhost', '127.0.0.1']
 
@@ -131,12 +131,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
-if not DEBUG:
+
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media fiels
 
